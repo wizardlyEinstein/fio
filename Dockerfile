468e1aae74052482d3235608015fe9aa7e0d5158
Leland Sindt <leland@sway.org>
2022-08-26 08:34:09 -0500

    raw

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,2 @@
-FROM alpine
-
-RUN apk add fio --repository=http://dl-cdn.alpinelinux.org/alpine/edge/community
+FROM alpine:3.16
+RUN apk add fio --repository=http://dl-cdn.alpinelinux.org/alpine/v3.16/community
---
+++ run.sh
@@ -0,0 +1,2 @@
+fio --name=fiotest --filename=./test1 --size=4Gb --rw=rw,readwrite --bs=8K --direct=0 --numjobs=8 --ioengine=libaio --iodepth=32 --group_reporting
+/usr/bin/fio --name=fiotest --filename=./test1 --size=4Gb --rw=rw,readwrite --bs=8K --direct=0 --numjobs=8 --ioengine=libaio --iodepth=32 --group_reporting
